fc5d694a36c8c8f32ed7e196038168d1ad4a5345
Vetor-Gomes <vitorvex@gmail.com>
2018-04-09 16:10:26 -0300

    Add files via upload

---
+++ programacao/app/controlers/categorias.php
@@ -0,0 +1,18 @@
+<?php
+if(isset($_GET['acao'])){
+    $acao = $_GET['acao'];
+}else{
+    $acao = 'index';
+}
+
+if($acao=='index'){
+    $crud= new CategoriaCrud();
+    $categorias = $crud->getCategorias();
+
+    include '../views/listaCategorias.php';
+}
+
+
+
+    include "../../rodape.html";
+?>
